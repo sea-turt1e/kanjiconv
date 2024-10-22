f65da6676c504c0d2c1462d641ea1b709d5c91ae
sea-turt1e <h.yamada.bg@gmail.com>
2024-10-22 10:31:33 +0900

    add:docstring

--- kanjiconv/kanjiconv.py
+++ kanjiconv/kanjiconv.py
@@ -1,13 +1,24 @@
 import json
 import os
+from typing import Callable
 
 import sudachipy
 
 from kanjiconv.entities import SudachiDictType
 
 
-def parse_text(func):
-    def wrapper(self, text):
+def parse_text(func: Callable) -> Callable:
+    """
+    Decorator function to tokenize text and convert it into readings before applying the given function.
+
+    Args:
+        func (Callable): The function to be wrapped, which takes the readings as input.
+
+    Returns:
+        Callable: Wrapped function that processes the readings.
+    """
+
+    def wrapper(self, text: str):
         tokens = self.tokenizer.tokenize(text)
         readings = self.separator.join([token.reading_form() for token in tokens])
         return func(self, readings)
@@ -16,24 +27,48 @@ def parse_text(func):
 
 
 class KanjiConv:
+    """
+    Class for converting Japanese text between different formats such as Hiragana, Katakana, and Roman characters.
+    """
+
     def __init__(
         self,
         sudachi_dict_type: SudachiDictType = SudachiDictType.FULL,
-        data_path="kanjiconv/data/kana.json",
+        data_path: str = "kanjiconv/data/kana.json",
         separator: str = " ",
-    ):
+    ) -> None:
+        """
+        Initializes the KanjiConv instance with a tokenizer and kana conversion data.
+
+        Args:
+            sudachi_dict_type (SudachiDictType): Type of the Sudachi dictionary to use for tokenization.
+            data_path (str): Path to the JSON file containing kana conversion data.
+            separator (str): Separator to use between token readings.
+        """
         assert os.path.isfile(data_path), "File does not exist"
         with open(data_path, "r") as f:
             self.kana = json.load(f)
             assert isinstance(self.kana, dict), "JSON must be a dictionary"
+
+        # Initialize Sudachi tokenizer
         sudachi_dict = sudachipy.Dictionary(dict=sudachi_dict_type.value)
         self.tokenizer = sudachi_dict.create()
         self.separator = separator
 
     @parse_text
     def to_hiragana(self, text: str) -> str:
+        """
+        Convert Katakana characters in the given text to Hiragana.
+
+        Args:
+            text (str): The input text containing Katakana characters.
+
+        Returns:
+            str: The converted text with Hiragana characters.
+        """
         hiragana_text = ""
         for char in text:
+            # Convert Katakana characters to Hiragana by adjusting their Unicode code point
             if "ァ" <= char <= "ン":
                 hiragana_text += chr(ord(char) - 0x60)
             else:
@@ -42,14 +77,32 @@ class KanjiConv:
 
     @parse_text
     def to_katakana(self, text: str) -> str:
+        """
+        Convert the given text to Katakana format.
+
+        Args:
+            text (str): The input text to be converted.
+
+        Returns:
+            str: The converted text in Katakana.
+        """
         return text
 
     @parse_text
     def to_roman(self, text: str) -> str:
+        """
+        Convert Katakana characters in the given text to their Romanized (Romaji) equivalents.
+
+        Args:
+            text (str): The input text containing Katakana characters.
+
+        Returns:
+            str: The Romanized representation of the input text.
+        """
         roman_text = ""
         i = 0
         while i < len(text):
-            # 2文字を取り出して拗音をチェック
+            # Check for combined small Katakana to handle contracted sounds (拗音)
             if (
                 i + 1 < len(text)
                 and text[i] in self.kana["full_katakana"]
@@ -58,10 +111,10 @@ class KanjiConv:
                 combined = text[i] + text[i + 1]
                 if combined in self.kana["katakana2roman"]:
                     roman_text += self.kana["katakana2roman"][combined]
-                    i += 2  # 拗音の場合は2文字を消費
+                    i += 2  # Consume two characters for contracted sounds
                     continue
 
-            # 通常のカタカナの処理
+            # Process normal Katakana characters
             char = text[i]
 
             if "ァ" <= char <= "ン" or "、" <= char <= "ー":
@@ -70,11 +123,3 @@ class KanjiConv:
                 roman_text += char
             i += 1
         return roman_text
-
-
-if __name__ == "__main__":
-    kanji_conv = KanjiConv(separator="/")
-    text = "幽☆遊☆白書は、最高の漫画デス。"
-    print(kanji_conv.to_hiragana(text))
-    print(kanji_conv.to_katakana(text))
-    print(kanji_conv.to_roman(text))
